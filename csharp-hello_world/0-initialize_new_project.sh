326c33c0f45d102614218677c46d80fadc896a95
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-04-29 01:58:01 -0500

    rm comments

--- csharp-hello_world/0-initialize_new_project.sh
+++ csharp-hello_world/0-initialize_new_project.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/bash
 
-# Create a new .NET console application project
-# The output directory is specified by the first argument, or defaults to "0-new_project"
 dotnet new console -o ${1:-"0-new_project"}
