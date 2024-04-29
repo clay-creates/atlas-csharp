603c2db6f090ed1911d40487e1bec617fcfabc6e
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-04-29 17:59:47 -0500

    capitolize WriteLine

--- csharp-hello_world/3-writeline/3-writeline.cs
+++ csharp-hello_world/3-writeline/3-writeline.cs
@@ -4,6 +4,6 @@ class Hello
 {
     static void Main()
     {
-        Console.Writeline("\"Programming is like building a multilingual puzzle");
+        Console.WriteLine("\"Programming is like building a multilingual puzzle");
     }
 }
