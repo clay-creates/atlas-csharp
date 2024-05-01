1baae020cf64302de348f8cd864e3b50dc13c8e9
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-05-01 23:51:01 +0000

    task 4

--- csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
+++ csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -6,7 +6,10 @@ namespace _4_print_hexa
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int i = 0; i < 99; i++)
+            {
+                Console.WriteLine($"{i} = 0x{i:x}");
+            }
         }
     }
 }
