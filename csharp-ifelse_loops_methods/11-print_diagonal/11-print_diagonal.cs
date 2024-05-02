3862cfe3b5ea429d7b3df7a9f624285ff9b577f8
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-05-02 03:04:48 +0000

    tester for task 11

---
+++ csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _11_print_diagonal
+{
+    public class Line
+    {
+        public static void PrintDiagonal(int length)
+        {
+            if (length <= 0)
+            {
+                Console.WriteLine();
+                return;
+            }
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write("\\");
+            }
+            Console.WriteLine();
+        }
+    }
+}
