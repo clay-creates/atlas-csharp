7b77c293519fb9a9a669de8c0903f0acf93573ba
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-05-02 02:15:05 +0000

    task 8

---
+++ csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _8_print_last_digit
+{
+    public class Number
+    {
+        public static int PrintLastDigit(int number)
+        {
+            int lastDigit = number % 10;
+            return lastDigit;
+        }
+    }
+}
