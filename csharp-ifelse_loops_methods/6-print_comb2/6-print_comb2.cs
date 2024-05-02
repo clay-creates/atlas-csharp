c0682431bfd7b1c5c5ef0f8a88da258fc9a50ca5
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-05-02 01:41:20 +0000

    task 6

---
+++ csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string combinations = "";
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = i + 1; j < 10; j++)
+                {
+                    combinations += $"{i}{j}, ";
+                }
+            }
+            combinations = combinations.TrimEnd(',', ' ');
+            Console.WriteLine(combinations);
+        }
+    }
+}
