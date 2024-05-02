21ee58539cc0da9e279f4fa1395ce1ac5f481328
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-05-02 02:57:17 +0000

    task 10

---
+++ csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _10_print_line
+{
+    public class Line
+    {
+        public static void PrintLine(int length)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                if (length <= 0)
+                {
+                    Console.WriteLine();
+                    return;
+                }
+                Console.Write("_");
+            }
+        }
+    }
+}
