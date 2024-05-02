95c2a0718e84d6b1936f2c8e29146fe86d99435d
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-05-02 03:17:55 +0000

    update

--- csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
+++ csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
@@ -4,9 +4,9 @@ namespace _12_fizzbuzz
 {
     public class FizzBuzz
     {
-        public static FizzBuzz(int i)
+        public static void PrintFizzBuzz()
         {
-            for (i = 1; i <= 100; i++)
+            for (int i = 1; i <= 100; i++)
             {
                 if (i % 3 == 0 && i % 5 == 0)
                 {
