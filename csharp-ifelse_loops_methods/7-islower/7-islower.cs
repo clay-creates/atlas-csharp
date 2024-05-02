32d48232c349c21932080804d01ad203a3427add
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-05-02 02:09:32 +0000

    task 7

---
+++ csharp-ifelse_loops_methods/7-islower/7-islower.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _7_islower
+{
+    public class Character
+    {
+        public static bool IsLower(char c)
+        {
+            return c >= 'a' && c <= 'z';
+        }
+    }
+}
