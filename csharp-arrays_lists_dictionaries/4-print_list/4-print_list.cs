8006e8774cac24ddf69a327ec43cea0813883d6a
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-05-03 16:43:25 +0000

    task 4

---
+++ csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+
+        List<int> numbers = new List<int>();
+
+        for (int i = 0; i < size; i++)
+        {
+            numbers.Add(i);
+        }
+
+        Console.WriteLine("List of integers:");
+        foreach (int number in numbers)
+        {
+            Console.Write(number + " ");
+        }
+        Console.WriteLine();
+
+        return numbers;
+    }
+}
