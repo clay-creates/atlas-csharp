f2016590aab7142ed46c08812b9b54457fe6c10c
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-05-03 18:51:13 +0000

    task 13

---
+++ csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        if (myList == null || myList.Count < 0)
+        {
+            return "None";
+        }
+        var bestScore = myList.OrderByDescending(pair => pair.Value).First().Key;
+        return myList;
+    }
+}
