017620fd74cfe19b6a87adfa43b19cfb293a3b19
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-05-03 17:00:01 +0000

    task 5

---
+++ csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class List
+{
+    public static int MaxInteger(List<int> myList)
+    {
+        if (myList == null || myList == 0)
+        {
+            Console.WriteLine("List is empty");
+            return -1;
+        }
+
+        int MaxInt = myList[0];
+
+        foreach (int num in myList)
+        {
+            if (num > MaxInt)
+            {
+                MaxInt = num;
+            }
+        }
+        return MaxInt;
+    }
+}
