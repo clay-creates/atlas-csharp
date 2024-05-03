3ac1dc6f66d1ebae5d7f66fcc9fd5a35cb09311a
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-05-03 17:02:46 +0000

    count check in nullcheck

--- csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
+++ csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 
 public class List
 {
     public static int MaxInteger(List<int> myList)
     {
-        if (myList == null || myList == 0)
+        if (myList == null || myList.Count == 0)
         {
             Console.WriteLine("List is empty");
             return -1;
