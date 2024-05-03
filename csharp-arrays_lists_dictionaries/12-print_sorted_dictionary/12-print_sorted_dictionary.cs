39034700934ca8a80ca0ceb2a4458dcb570fba15
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-05-03 18:34:43 +0000

    task 12

---
+++ csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Dictionary
+{
+    public static void PrintSorted(Dictionary<string, string> myDict)
+    {
+        var sortedKeys = myDict.Keys.OrderBy(sortedKeys => sortedKeys).ToList();
+        foreach (var key in sortedKeys)
+        {
+            Console.WriteLine($"{key}: {myDict[key]}");
+        }
+    }
+}
