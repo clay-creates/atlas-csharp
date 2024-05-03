992a70c096db202a49b61efc29aa2cf5dfa87eff
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-05-03 19:04:43 +0000

    task 14

---
+++ csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[,] array = new int[5, 5];
+
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array[i, j] = 0;
+            }
+        }
+
+        array[2, 2] = 1;
+
+        for (int i = 0; array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                Console.Write(array[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
