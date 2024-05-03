fe6a0cc99543ef68078e945c72c08af09eda999a
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-05-03 15:33:55 +0000

    task 0

---
+++ csharp-array_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _0_print_array
+{
+    public class Array
+    {
+        public static int[] CreatePrint(int size)
+        {
+            if (size < 0)
+            {
+                Console.WriteLine("Size cannot be negative");
+                return null;
+            }
+            int[] array = new int[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = i;
+            }
+
+            if (size == 0)
+            {
+                Console.Writeline();
+            }
+            else
+            {
+                Console.WriteLine(string.Join(", ", array));
+            }
+            return array;
+        }
+    }
+}
