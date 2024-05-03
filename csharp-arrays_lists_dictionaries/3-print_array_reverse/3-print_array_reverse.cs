85531c3195b0696f80407e7879750b0dcd6db63c
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-05-03 16:16:52 +0000

    task 3

---
+++ csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Array
+{
+    public static void Reverse(int[] array)
+    {
+        for (int i = array.Length - 1; i >= 0; i--)
+        {
+            Console.Write(array[i]);
+        }
+    }
+}
