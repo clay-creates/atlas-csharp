88c90acec5391a8a9a2a2470c7fc581ea4eb1505
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-05-04 23:52:21 +0000

    public class?

--- csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-class MyStack
+public class MyStack
 {
     public static MyStack<string> Info(MyStack<string> aStack, string newItem, string search)
     {
