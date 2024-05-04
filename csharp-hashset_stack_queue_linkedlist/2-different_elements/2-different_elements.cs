885a40ed1d9f09ab87d3cd2ab08fa398bd535f9f
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-05-04 23:24:47 +0000

    rewrite

--- csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
+++ csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -5,36 +5,23 @@ class List
 {
     public static List<int> DifferentElements(List<int> list1, List<int> list2)
     {
-        HashSet<int> set1 = new HashSet<int>();
-        HashSet<int> set2 = new HashSet<int>();
-
         List<int> differences = new List<int>();
         foreach (int element in list1)
         {
-            if (!set2.Contains(element))
+            if (!list2.Contains(element))
             {
                 differences.Add(element);
             }
         }
+
         foreach (int element in list2)
         {
-            if (!set1.Contains(element))
+            if (!list1.Contains(element))
             {
                 differences.Add(element);
             }
         }
-        // for (int i = 0; i < differences.Count - 1; i++)
-        // {
-        //     for (int j = 0; j < differences.Count - i - 1; j++)
-        //     {
-        //         if (differences[j] > differences[j + 1])
-        //         {
-        //             int temp = differences[j];
-        //             differences[j] = differences[j + 1];
-        //             differences[j + 1] = temp;
-        //         }
-        //     }
-        // }
+
         differences.Sort();
         return differences;
     }
