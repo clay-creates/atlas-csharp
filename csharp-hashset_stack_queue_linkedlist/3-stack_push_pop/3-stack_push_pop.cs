3054b49e9d8f0e2563c080ee62cbaf152b3d50bf
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-05-04 23:48:51 +0000

    task 3

---
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+class MyStack
+{
+    public static MyStack<string> Info(MyStack<string> aStack, string newItem, string search)
+    {
+        Console.WriteLine($"Number of items: {aStack.Count}");
+
+        if (aStack.Count > 0)
+        {
+            Console.WriteLine($"Top item: {aStack.Peek()}");
+        }
+        else
+        {
+            Console.WriteLine("Stack is empty");
+        }
+
+        bool containSearch = aStack.Contains(search);
+        Console.WriteLine($"Stack contains {search}: {containSearch}");
+
+        if (containSearch)
+        {
+            while (aStack.Count > 0 && aStack.Peek() != search)
+            {
+                aStack.Pop();
+            }
+
+            if (aStack.Count > 0)
+            {
+                aStack.Pop();
+            }
+        }
+
+        aStack.Push(newItem);
+        return aStack;
+    }
+}
