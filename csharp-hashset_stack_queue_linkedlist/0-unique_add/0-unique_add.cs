f3dff8aaa6a8745b588efdbc509fa1416fdd7def
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-05-04 19:57:48 +0000

    task 0

---
+++ csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+        HashSet<int> uniqInt = new HashSet<int>();
+        foreach (int number in myList)
+        {
+            uniqInt.Add(number);
+        }
+        int sum = 0;
+        foreach (int number in uniqInt)
+        {
+            sum += number;
+        }
+        return sum;
+    }
+}
---
+++ csharp-hashset_stack_queue_linkedlist/README.md
@@ -0,0 +1,1 @@
+# C# - Hashsets, Stacks, Queues & Linked Lists
