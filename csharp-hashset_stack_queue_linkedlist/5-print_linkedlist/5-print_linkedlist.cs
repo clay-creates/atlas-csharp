aca40855c5724cef57f608d205271ae466fb6b39
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-05-05 00:22:32 +0000

    task 5

---
+++ csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            return new LinkedList<int>();
+        }
+
+        LinkedList<int> list = new LinkedList<int>();
+
+        for (int i = 0; i <= size; i++)
+        {
+            list.AddLast(i);
+        }
+
+        Console.WriteLine("LinkedList:");
+        foreach (var item in list)
+        {
+            Console.Write(item + " ");
+        }
+        Console.WriteLine();
+        return list;
+    }
+}
