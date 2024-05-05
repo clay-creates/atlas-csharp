f14eea0ff0c7874e8abf057a020e45144c9428b8
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-05-05 19:10:30 +0000

    file setup, task 0

---
+++ csharp-structs_enums/0-dog/0-dog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _0_dog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            enum Rating
+        {
+            Good,
+            Great,
+            Excellent,
+        }
+    }
+}
+}
