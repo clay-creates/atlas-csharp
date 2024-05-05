e07c6153fef40d383dd196653e33b75b4a727dcc
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-05-05 21:28:12 +0000

    task 3

--- csharp-classes/3-enemy/3-enemy.cs
+++ csharp-classes/3-enemy/3-enemy.cs
@@ -1,12 +1,42 @@
 ﻿using System;
 
-namespace _3_enemy
+namespace Enemies
 {
-    class Program
+    /// <summary>
+    /// This class represents a zombie enemy.
+    /// </summary>
+    public class Zombie
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// This variable creates the health field for zombies.
+        /// </summary>
+        public int health;
+        /// <summary>
+        /// Creates new instance of Zombie class.
+        /// </summary>
+        public Zombie()
         {
-            Console.WriteLine("Hello World!");
+            this.health = 0;
+        }
+        /// <summary>
+        /// Public constructor of Zombie.
+        /// </summary>
+        /// <param name="value"></param>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            this.health = value;
+        }
+        /// <summary>
+        /// Returns health of Zombie object
+        /// </summary>
+        /// <returns></returns>
+        public int GetHealth()
+        {
+            return this.health;
         }
     }
 }
