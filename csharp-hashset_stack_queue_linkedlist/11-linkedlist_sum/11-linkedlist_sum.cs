4155a7cf346196466a4508c84bcb92370f450fe9
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-05-05 17:25:51 +0000

    task 11

---
+++ csharp-hashset_stack_queue_linkedlist/11-linkedlist_sum/11-linkedlist_sum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+class LList
+{
+    public static int Sum(LinkedList<int> myLList)
+    {
+        int listSum = 0;
+        foreach (int value in myLList)
+        {
+            listSum += value;
+        }
+        return listSum;
+    }
+}
