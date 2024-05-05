0f724cd2c6e5fae6a9a908e70e3f84a415da50a9
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-05-05 18:25:11 +0000

    file setup, task 1

---
+++ csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int SafePrint(List<int> myList, int n)
+    {
+        int printed = 0;
+        try
+        {
+            for (int i = 0; i < Math.Min(n, myList.Count); i++)
+            {
+                Console.WriteLine(myList[i]);
+                printed++;
+            }
+        }
+        catch (IndexOutOfRangeException)
+        {
+            return 0;
+        }
+        return printed;
+    }
+}
---
+++ csharp-exceptions/4-throw_excpetions_msg/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _4_throw_excpetions_msg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
