1be013f9ab057e722f7492750737f949187fc2e4
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-05-05 16:29:58 +0000

    task 6

---
+++ csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Length(LinkedList<int> myLList)
+    {
+        int lengthCount = 0;
+        foreach (var item in myLList)
+        {
+            lengthCount++;
+        }
+        return lengthCount;
+    }
+}
