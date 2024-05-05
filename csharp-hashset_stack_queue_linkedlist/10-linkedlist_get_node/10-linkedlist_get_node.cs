072d5d53be882f35bcd7e05d3905c9b560b72650
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-05-05 17:18:02 +0000

    task 10

---
+++ csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        if (myLList == null || myLList.Count == 0)
+        {
+            return 0;
+        }
+
+        if (n < 0 || n >= myLList.Count)
+        {
+            return 0;
+        }
+
+        int index = 0;
+        foreach (int item in myLList)
+        {
+            if (index == n)
+            {
+                return item;
+            }
+            item++;
+        }
+        return 0;
+    }
+}
