1e7111eb62f324aaf09d762b62f79d9ebb04dced
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-05-05 17:44:17 +0000

    task 13

---
+++ csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        LinkedListNode<int> nodeToDelete = myLList.First;
+        for (int i = 0; i < index; i++)
+        {
+            nodeToDelete = nodeToDelete.Next;
+        }
+
+        myLList.Remove(nodeToDelete);
+    }
+}
