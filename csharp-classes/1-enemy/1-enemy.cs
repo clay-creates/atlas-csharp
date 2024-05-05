e17ae3121d2e1bb287a6c4cd99ae71f702ef9bb2
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-05-05 21:17:16 +0000

    xml doc

--- csharp-classes/1-enemy/1-enemy.cs
+++ csharp-classes/1-enemy/1-enemy.cs
@@ -7,7 +7,13 @@ namespace Enemies
     /// </summary>
     public class Zombie
     {
+        /// <summary>
+        /// This variable creates the health field for zombies.
+        /// </summary>
         public int health;
+        /// <summary>
+        /// Creates new instance of Zombie class.
+        /// </summary>
         public Zombie()
         {
             health = 0;
