c621bf7cbd74e3f09aa2e4d53687550ce6898461
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-05-05 18:30:46 +0000

    task 1

---
+++ csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Int
+{
+    public static void divide(int a, int b)
+    {
+        double result = 0;
+        try
+        {
+            result = (double)a / b;
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Error: Division by zero");
+        }
+        finally
+        {
+            Console.WriteLine($"The result of {a} divided by {b} is: {result}");
+        }
+    }
+}
