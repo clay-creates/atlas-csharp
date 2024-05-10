3bdfeba893691cec03da6a143e41000a21945ad4
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-05-10 16:04:20 +0000

    xml?

--- csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
+++ csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
@@ -9,28 +9,39 @@ namespace Tests
         [SetUp]
         public void Setup()
         {
-            int a = 1;
-            int b = 2;
         }
 
+
+        /// <summary>
+        /// Test the addition of two positive integers
+        /// </summary>
         [Test]
         public void AddTwoPositive()
         {
             Assert.AreEqual(3, Operations.Add(1, 2), "The sum of 1 and 2 should be 3");
         }
 
+        /// <summary>
+        /// Test the addition of two negative integers
+        /// </summary>
         [Test]
         public void AddTwoNegative()
         {
             Assert.AreEqual(-3, Operations.Add(-1, -2), "The sum of -1 and -2 should be -3");
         }
 
+        /// <summary>
+        /// Test the addition of two zero integers
+        /// </summary>
         [Test]
         public void AddTwoZero()
         {
             Assert.AreEqual(0, Operations.Add(0, 0), "The sum of 0 and 0 should be 0");
         }
 
+        /// <summary>
+        /// Test the addition of one positive and one negative integer
+        /// </summary>
         [Test]
         public void AddOnePositive_OneNegative()
         {
