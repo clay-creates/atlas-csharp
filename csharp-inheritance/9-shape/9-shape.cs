732c475b81fa1f4eb1042fd3c7ae96ad221bb3e1
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-05-12 20:48:08 +0000

    task 9

--- csharp-inheritance/9-shape/9-shape.cs
+++ csharp-inheritance/9-shape/9-shape.cs
@@ -1,12 +1,78 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace _9_shape
+class Shape
 {
-    class Program
+    public virtual int Area()
     {
-        static void Main(string[] args)
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
+
+class Rectangle : Shape
+{
+    private int width;
+    private int height;
+    public int Width
+    {
+        get { return width; }
+        set
         {
-            Console.WriteLine("Hello World!");
+            if (value < 0)
+            {
+                throw new ArgumentException("Width must be greater than or equal to 0");
+            }
+            else
+            {
+                width = value;
+            }
         }
+    }
+    public int Height
+    {
+        get { return height; }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Height must be greater than or equal to 0");
+            }
+            else
+            {
+                height = value;
+            }
+        }
+    }
+    public new int Area()
+    {
+        return width * height;
+    }
+    public override string ToString()
+    {
+        return $"[Rectangle] {width} / {height}";
+    }
+}
+
+class Square : Rectangle
+{
+    private int size;
+    public int Size
+    {
+        get { return size; }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Size must be greater than or equal to 0");
+            }
+            else
+            {
+                size = value;
+            }
+        }
+    }
+    public override string .ToString()
+    {
+        return $"[Square] {size} / {size}";
     }
 }
