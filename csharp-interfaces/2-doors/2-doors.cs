acec3e73d5a97a71bcff7b95a1e091569aa777d0
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-05-14 20:18:38 +0000

    xml comments

--- csharp-interfaces/2-doors/2-doors.cs
+++ csharp-interfaces/2-doors/2-doors.cs
@@ -1,37 +1,85 @@
 ﻿using System;
 
+/// <summary>
+/// Represents a base class for objects that have a name.
+/// </summary>
 public abstract class Base
 {
+    /// <summary>
+    /// Gets or sets the name of the object.
+    /// </summary>
     public string name { get; set; }
+
+    /// <summary>
+    /// Returns a string representation of the object.
+    /// </summary>
+    /// <returns>A string that represents the current object.</returns>
     public override string ToString()
     {
         return $"{name} is a {this.GetType()}";
     }
 }
 
+/// <summary>
+/// Defines a contract for interactive objects.
+/// </summary>
 public interface IInteractive
 {
+    /// <summary>
+    /// Performs an interaction with the object.
+    /// </summary>
     void Interact();
 }
 
+/// <summary>
+/// Defines a contract for breakable objects.
+/// </summary>
 public interface IBreakable
 {
+    /// <summary>
+    /// Gets the durability of the object.
+    /// </summary>
     int durability { get; set; }
+
+    /// <summary>
+    /// Breaks the object.
+    /// </summary>
     void Break();
 }
 
+/// <summary>
+/// Defines a contract for collectable objects.
+/// </summary>
 public interface ICollectable
 {
+    /// <summary>
+    /// Gets a value indicating whether the object has been collected.
+    /// </summary>
     bool isCollected { get; set; }
+
+    /// <summary>
+    /// Collects the object.
+    /// </summary>
     void Collect();
 }
 
+/// <summary>
+/// Represents a door that can be interacted with.
+/// </summary>
 class Door : Base, IInteractive
 {
+    /// <summary>
+    /// Initializes a new instance of the Door class.
+    /// </summary>
+    /// <param name="name">The name of the door.</param>
     public Door(string name = "Door")
     {
         this.name = name;
     }
+
+    /// <summary>
+    /// Interacts with the door.
+    /// </summary>
     public void Interact()
     {
         Console.WriteLine($"You try to open the {this.name}. It's locked.");
