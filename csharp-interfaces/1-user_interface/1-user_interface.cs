c0ef410cbef34f134877757e1992321fddf48c41
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-05-14 16:54:39 +0000

    task 1

--- csharp-interfaces/1-user_interface/1-user_interface.cs
+++ csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 
-namespace _1_user_interface
+abstract class Base
 {
-    class Program
+    public string name = "";
+    public override string ToString()
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        return $"{name} is a {this.GetType().Name}";
     }
+}
+
+public interface IInteractive
+{
+    void Interact();
+}
+
+public interface IBreakable
+{
+    public int durability;
+    void Break();
+}
+
+public interface ICollectable
+{
+    public bool isCollected;
+    void Collect();
+}
+
+class TestObject : Base : IInteractive: IBreakable: ICollectable
+{
+
 }
