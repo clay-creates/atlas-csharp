9b2a934f72406af7d900b89195ad3792c6ed60e7
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-05-14 16:43:55 +0000

    from == to =

--- csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
+++ csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -2,7 +2,7 @@
 
 abstract class Base
 {
-    public string name == "";
+    public string name = "";
     public override string ToString()
     {
         return $"{name} is a {this.GetType().Name}";
