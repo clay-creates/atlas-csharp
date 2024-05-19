54cf14874445f37884088a808ae34695528b5a81
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-05-19 19:08:01 +0000

    task 3 first pass

--- csharp-generics/3-peek/queue.cs
+++ csharp-generics/3-peek/queue.cs
@@ -1,12 +1,133 @@
 ﻿using System;
 
-namespace _3_peek
+/// <summary>
+/// A custom generic queue
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public class Queue<T>
 {
-    class Program
+    /// <summary>
+    /// The head node of our queue
+    /// </summary>
+    private Node head;
+    /// <summary>
+    /// The tail node of our queue
+    /// </summary>
+    private Node tail;
+    /// <summary>
+    /// Count of nodes in queue
+    /// </summary>
+    public int count = 0;
+    /// <summary>
+    /// A node in our queue
+    /// </summary>
+
+    public class Node
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// Gets or sets the value of the current node
+        /// </summary>
+        public T Value { get; set; }
+        /// <summary>
+        /// Gets the next node in the queue
+        /// </summary>
+        public Node Next { get; set; }
+        /// <summary>
+        /// Initializes a new instance of Node class
+        /// </summary>
+        /// <param name="value"></param>
+        public Node(T value)
         {
-            Console.WriteLine("Hello World!");
+            Value = value;
+            Next = null;
         }
+    }
+
+    /// <summary>
+    /// Checks the type of the items in queue
+    /// </summary>
+    /// <returns>Returns the type of our T items</returns>
+    public Type CheckType()
+    {
+        return typeof(T);
+    }
+
+    /// <summary>
+    /// Initialized a new instance of our Queue
+    /// </summary>
+    public Queue()
+    {
+        head = null;
+        tail = null;
+    }
+
+    /// <summary>
+    /// Add item to end of queue
+    /// </summary>
+    /// <param name="item"></param>
+    public void Enqueue(T item)
+    {
+        Node NewNode = new Node(item);
+        count++;
+        if (tail == null)
+        {
+            head = NewNode;
+            tail = NewNode;
+        }
+        else
+        {
+            tail.Next = NewNode;
+            tail = NewNode;
+        }
+    }
+
+    /// <summary>
+    /// Returns and removes the head item of the queue
+    /// </summary>
+    /// <returns></returns>
+    public T Dequeue()
+    {
+        if (head == null)
+        {
+            throw new InvalidOperationException("Queue is empty");
+        }
+        T value = head.Value;
+        head = head.Next;
+        if (head == null)
+        {
+            tail = null;
+        }
+        count--;
+        return value;
+    }
+
+    /// <summary>
+    /// Show head item
+    /// </summary>
+    /// <returns></returns>
+    public T Peek()
+    {
+        if (head == null)
+        {
+            throw new InvalidOperationException("Queue is empty");
+        }
+        return head.Value;
+    }
+
+    /// <summary>
+    /// Checks if the queue is empty or not
+    /// </summary>
+    /// <returns></returns>
+    public bool IsEmpty()
+    {
+        return head == null;
+    }
+    /// <summary>
+    /// Returns the number of nodes in queue
+    /// </summary>
+    /// <returns></returns>
+    public int Count()
+    {
+        return count;
     }
 }
