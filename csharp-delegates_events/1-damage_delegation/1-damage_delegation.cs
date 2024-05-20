a20da0746f1e407bbec98d26ad01ec7341cd1946
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-05-20 18:22:57 +0000

    task 1 first pass

---
+++ csharp-delegates_events/1-damage_delegation/1-damage_delegation.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+/// <summary>
+/// class representing a player character
+/// </summary>
+public class Player
+{
+    /// <summary>
+    /// Gets or sets the name of the player
+    /// </summary>
+    /// <value>name string</value>
+    private string name { get; set; }
+    /// <summary>
+    /// Gets or sets the value of player max hp
+    /// </summary>
+    /// <value>float health value</value>
+    private float maxHp { get; set; }
+    /// <summary>
+    /// Gets or sets the value of player current hp
+    /// </summary>
+    /// <value>float current hp value</value>
+    private float hp { get; set; }
+
+    /// <summary>
+    /// player constructor creates instance of player class
+    /// </summary>
+    /// <param name="name">default "Player"</param>
+    /// <param name="maxHp">default 100f</param>
+    public Player(string name = "Player", float maxHp = 100f)
+    {
+        this.name = name;
+
+        if (maxHp <= 0)
+        {
+            maxHp = 100f;
+            System.Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+        }
+
+        this.maxHp = maxHp;
+        this.hp = maxHp;
+    }
+    /// <summary>
+    /// prints the players current health
+    /// </summary>
+    public void PrintHealth()
+    {
+        System.Console.WriteLine($"{name} has {hp} / {maxHp} health");
+    }
+
+    public delegate void CalculateHealth(float amount);
+
+    public void TakeDamage(float damage)
+    {
+        System.Console.WriteLine($"{name} takes {damage} damage!");
+        Player.hp -= damage;
+        if (damage <= 0)
+        {
+            System.Console.WriteLine($"{name} takes 0 damage!");
+        }
+    }
+
+    public HealDamage(float heal)
+    {
+        System.Console.WriteLine($"{name} heals {heal} HP!");
+        Player.hp += heal;
+        if (heal <= 0)
+        {
+            System.Console.WriteLine($"{name} heals 0 HP!");
+        }
+    }
+}
