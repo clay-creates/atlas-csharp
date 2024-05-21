a883378b68d9452f35a8dccebfde08db9dd3b436
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-05-21 18:54:38 +0000

    task 6

---
+++ csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class VectorMath
+{
+    public static double[] Add(double[] vector1, double[] vector 2)
+    {
+        if (vector1.Length == 2 && vector2.Length == 2)
+        {
+            return new double[]
+            {
+                vector1[0] + vector2[0],
+                vector1[1] + vector2[1],
+            };
+        }
+        else if (vector1.Length == 3 && vector2.Length == 3)
+        {
+            return new double[]
+            {
+                vector1[0] + vector2[0],
+                vector1[1] + vector2[1],
+                vector1[2] + vector2[2],
+            };
+        }
+        else
+        {
+            return new double[] { -1 };
+        }
+    }
+}
