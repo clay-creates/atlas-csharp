2ebf5cb5a17e8e878f253698d9ad85b54f3e17fc
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-05-21 18:35:50 +0000

    rm namespace

--- csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -1,24 +1,21 @@
 ﻿using System;
 
-namespace _3_magnitude
+class VectorMath
 {
-    class VectorMath
+    public static double Magnitude(double[] vector)
     {
-        public static double Magnitude(double[] vector)
+        if (vector.Length != 2 && vector.Length != 3)
         {
-            if (vector.Length != 2 && vector.Length != 3)
-            {
-                return -1;
-            }
-
-            double sum = 0;
-            for (int i = 0; i < vector.Length; i++)
-            {
-                sum += Math.Pow(vector[i], 2);
-            }
-
-            double magnitude = Math.Sqrt(sum);
-            return Math.Round(magnitude);
+            return -1;
         }
+
+        double sum = 0;
+        for (int i = 0; i < vector.Length; i++)
+        {
+            sum += Math.Pow(vector[i], 2);
+        }
+
+        double magnitude = Math.Sqrt(sum);
+        return Math.Round(magnitude);
     }
 }
