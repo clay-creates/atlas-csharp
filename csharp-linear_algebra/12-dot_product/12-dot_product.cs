487523eb626ee56c0f350e2b44c37faad36b7ecb
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-05-21 20:41:27 +0000

    task 12

---
+++ csharp-linear_algebra/12-dot_product/12-dot_product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class VectorMath
+{
+    public static double DotProduct(double[] vector1, double[] vector2)
+    {
+        if (vector1.Length != vector2.Length || vector1.Length < 2 || vector2.Length < 2)
+        {
+            return -1;
+        }
+
+        double result = 0;
+        for (int i = 0; i < vector1.Length; i++)
+        {
+            result += vector1[i] * vector2[i];
+        }
+        return result;
+    }
+}
