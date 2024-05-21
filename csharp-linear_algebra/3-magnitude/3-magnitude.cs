57e7632301e2a333dc1cf2aef6414ce38d637ce0
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-05-21 18:33:01 +0000

    file setup, task 3

---
+++ csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Math;
+
+namespace _3_magnitude
+{
+    class VectorMath
+    {
+        public static double Magnitude(double[] vector)
+        {
+            if (vector.Length != 2 && vector.Length != 3)
+            {
+                return -1;
+            }
+
+            double sum = 0;
+            for (int i = 0; i < vector.Length; i++)
+            {
+                sum += Math.Pow(vector[i], 2);
+            }
+
+            double magnitude = Math.Sqrt(sum);
+            return Math.Round(magnitude);
+        }
+    }
+}
