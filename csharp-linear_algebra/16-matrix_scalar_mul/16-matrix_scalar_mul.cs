4d2697412af632d3bb5d5566f6abfb1054c5bda2
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-05-21 21:03:58 +0000

    task 16

---
+++ csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class MatrixMath
+{
+    public static double[,] MultiplyScalar(double[,] matrix, double scalar)
+    {
+        if (matrix.GetCount(0) == 2 || matrix.GetCount(0) == 3)
+        {
+            double[,] result = new double[];
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                result += matrix[i] * scalar;
+            }
+        }
+        else
+        {
+            return new double[-1, -1];
+        }
+    }
+}
