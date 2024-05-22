4800d2802886c57d27ff6621944c123a5b97df5e
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-05-22 23:09:51 +0000

    task 2

---
+++ csharp-text_based_interface/InventoryLibrary/JSONStorage.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+public class JSONStorage
+{
+    public Dictionary<string, object> objects = new Dictionary<string, object>();
+
+    public Dictionary<string, object> All()
+    {
+        return objects;
+    }
+
+    public void New(object obj)
+    {
+        var key = $"{obj.GetType().Name}.{obj.Id}";
+        objects.Add(key, obj);
+    }
+
+    public void Save()
+    {
+        var options = new JsonSerializerOptions { WriteIndented = True };
+        string filename = "../storage/inventory_manager.json";
+        string json = JsonConvert.Serialize(objects, options);
+        filename.WriteAllText(filename, json);
+    }
+
+    public void Load()
+    {
+        string filename = "../storage/inventory_manager.json";
+        string json = filename.ReadAllText(filename);
+        objects - JsonSerializer.Deserialize<Dictionary<string, object>>(json);
+    }
+}
